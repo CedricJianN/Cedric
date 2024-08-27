1d86ece28e6c05499e0a6a44fd6fa70d21739a7f
Cedric Jian N. Martin <cjnmartin.student@ua.edu.ph>
2024-08-27 12:16:40 +0800

    <Third Commit>

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,22 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I am Cedric. I am Software Engineer</p>
+        <p>Hi I am <strong>Cedric Jian N. Martin.</strong> I am 21 years old, and my hobbies are playing video games and cooking, my favorite genre music are R&B and POP, and lastly my favorite food is Tinola. </p>
+
+        <hr></hr>
+
+        <p>The reason I take IT course is, I was exposed to computers and tech gadgets. I chose IT to further more improve my skills.
+        </p>
+
+        <hr></hr>
+
+        <p> The career I see to my self after graduation, is to become a Software Developer. </p>
+
+        <hr></hr>
+
+        <p>I expect learn more about Programming and Coding, so that it can help me improve myself or add into my knowledge.</p>
+
+        
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
